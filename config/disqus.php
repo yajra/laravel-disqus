dfa0486a7bf4cf26c3a15a83fdeaadd1d8fbfc77
Arjay Angeles <aqangeles@gmail.com>
2017-11-24 05:03:58 +0000

    Apply fixes from StyleCI

--- config/disqus.php
+++ config/disqus.php
@@ -1,13 +1,13 @@
 <?php
 
 return [
-    /**
+    /*
      * If set to true, disqus api will be integrated in the site.
      * Just add <div id="disqus_thread"></div> to load disqus section.
      */
     'enabled'  => env('DISQUS_ENABLED', false),
 
-    /**
+    /*
      * Your disqus username.
      */
     'username' => env('DISQUS_USERNAME', ''),
